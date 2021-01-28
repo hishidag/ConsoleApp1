700606a41239cf985ec9628b824ac80a73ed2908
紘隆 石田 <hishidag@gmail.com>
2021-01-28 10:16:59 +0900

    add Rule.cs

---
+++ Rule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    // Chain of Responsibility
+    abstract class Rule
+    {
+        Rule nextRule;
+        protected string info;
+
+        public Rule(Rule next)
+        {
+            this.nextRule = next;
+        }
+
+        abstract public bool CanApply();
+
+        public string Apply()
+        {
+            if (CanApply()) return info;
+            return nextRule.Apply();
+        }
+
+    }
+
+
+
+}
