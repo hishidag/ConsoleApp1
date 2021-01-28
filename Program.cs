b2dccd8b8a07182f14e94ef71fc389c07e469ee4
紘隆 石田 <hishidag@gmail.com>
2021-01-28 10:10:41 +0900

    add rules

--- Program.cs
+++ Program.cs
@@ -13,6 +13,24 @@ namespace ConsoleApp1
             {
                 string guess = Console.ReadLine();
 
+                if(!int.TryParse(guess, out int _))
+                {
+                    Console.WriteLine("数字を入力してください");
+                    continue;
+                }
+
+                if (secret.Length != guess.Length)
+                {
+                    Console.WriteLine($"シークレットは{secret.Length}桁です");
+                    continue;
+                }
+
+                if (secret.Equals(guess))
+                {
+                    Console.WriteLine("Bingo!!!");
+                    continue;
+                }
+
                 int bulls = 0, cows = 0;
                 int[] count = new int[10];
 
