5c8102266fadf91d0f1a7587499f17cdae8ab0de
紘隆 石田 <hishidag@gmail.com>
2021-01-29 14:33:37 +0900

    secretの作成方法追加

--- BullsAndCowsGame/Game.cs
+++ BullsAndCowsGame/Game.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BullsAndCows
 {
@@ -31,6 +33,21 @@ namespace BullsAndCows
         private int NewSecretNumber()
         {
             return new Random().Next(0, (int)Math.Pow(10,4));
+        }
+
+        private int NewDistinctSecretNumber()
+        {
+            int num = 0;
+            List<int> numlist = Enumerable.Range(0, 10).ToList();
+
+            while(numlist.Count > 0)
+            {
+                int takeIndex = new Random().Next(0, numlist.Count);
+                num = 10 * num + numlist[takeIndex];
+                numlist.RemoveAt(takeIndex);
+            }
+
+            return num;
         }
 
     }
